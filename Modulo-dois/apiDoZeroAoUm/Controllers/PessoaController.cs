37d3584b487270ec5cb733545eed085af8fcf825
Diego Gonzales <diegogonzales@outlook.com.br>
2021-04-28 01:31:41 -0300

    Add Api do zero ao um

---
+++ Modulo-dois/apiDoZeroAoUm/Controllers/PessoaController.cs
@@ -0,0 +1,32 @@
+﻿using apiDoZeroAoUm.Data;
+using apiDoZeroAoUm.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiDoZeroAoUm.Controllers
+{
+ 
+    [ApiController]
+    [Route("v1/pessoas")] //Mesma função do RequestMapping("/v1/pessoas")
+    public class PessoaController : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Pessoa>> Insert([FromServices] DataContext context, [FromBody] Pessoa model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Pessoas.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
---
+++ Modulo-dois/apiDoZeroAoUm/Models/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiDoZeroAoUm.Models
+{
+    public class Pessoa
+    {
+       [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(30, ErrorMessage = "Este campo deve possuir no máximo 30 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve possuir no minimo 3 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(0, 99, ErrorMessage = "Entre com um numero valido")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(30, ErrorMessage = "Este campo deve possuir no maximo 20 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve possuir no minimo 3 caracteres")]
+        public string Sexo { get; set; }
+    }
+}
