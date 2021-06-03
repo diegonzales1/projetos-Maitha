a2f985f7bb0980ca3ccc2e0c8ced70f3ae08a480
Diego Gonzales <diegogonzales@outlook.com.br>
2021-06-03 18:51:46 -0300

    Removed folder

--- Modulo-dois/Nova pasta/ApiPessoa/SQLQuery1.sql
+++
@@ -1,9 +0,0 @@
-use ApiPessoa
-
-insert into Pessoas (Nome, Idade, Sexo) values ('Diego', 25, 'Masculino');
-
-select * from Pessoas;
-
-insert into Profissionais (Funcao, Num_carteira, Pessoa_id) values ('Starter', 124523, 1);
-
-select * from Profissionais;
